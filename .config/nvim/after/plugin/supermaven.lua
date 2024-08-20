2da4e2f2c025a7f6aba005806123be3a554da3a1
MaxTheTurtle0 <m.andrei.antonescu@gmail.com>
2024-08-20 19:50:24 +0300

    remove supermaven config

--- .config/nvim/after/plugin/supermaven.lua
+++
@@ -1,8 +0,0 @@
-require("supermaven-nvim").setup({
-    keymaps = {
-        accept_suggestion = "<Tab>",
-        clear_suggestion = "<C-k>",
-        accept_word = "<C-j>",
-    },
-    disable_keymaps = false -- disables built in keymaps for more manual control
-})
